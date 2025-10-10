be1a3319c3968bc35dc4245d2a999e0a9263ab09
ebotfabien <touchone0001@gmail.com>
2025-10-10 12:27:44 +0200

    linear_regression

--- api/app/ml_model.py
+++ api/app/ml_model.py
@@ -4,6 +4,7 @@ import pickle
 # Load your pre-trained model
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
+    print(model.feature_names_in_)
 
 def predict(df: pd.DataFrame) -> pd.Series:
     """
