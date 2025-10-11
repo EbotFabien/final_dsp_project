cdfd25d603b796b7c1770e1e1ee7df258b5fbe73
ebotfabien <touchone0001@gmail.com>
2025-10-11 08:30:03 +0200

    requirements_done

--- api/app/main.py
+++ api/app/main.py
@@ -5,6 +5,8 @@ from ml_model import predict
 from database import SessionLocal, engine, Base
 from crud import save_prediction, get_predictions
 import pandas as pd
+from typing import List
+from fastapi import Query
 
 # Create tables
 Base.metadata.create_all(bind=engine)
@@ -30,18 +32,23 @@ def predict_single(input_data: HouseInput, db: Session = Depends(get_db)):
     save_prediction(db, df.to_dict(orient="records")[0], source="webapp")
     return df.to_dict(orient="records")[0]
 
-@app.post("/predict_csv/")
-async def predict_csv():
-    pass
-    '''file: UploadFile = File(...), db: Session = Depends(get_db)):
-    contents = await file.read()
-    df = pd.read_csv(pd.io.common.BytesIO(contents))
-    prediction = predict(df)
-    df["price"] = prediction
-    # Save each prediction
-    for row in df.to_dict(orient="records"):
-        save_prediction(db, row, source="webapp")
-    return df.to_dict(orient="records")'''
+@app.post("/predict_batch/")
+def predict_batch(inputs: List[HouseInput],source: str = Query("webapp"),db: Session = Depends(get_db)):
+    # Convert all input dicts into a DataFrame
+    df = pd.DataFrame([i.dict() for i in inputs])
+
+    # Make batch predictions
+    predictions = predict(df)
+    df["price"] = predictions
+
+    # Save each prediction to the DB
+    for record in df.to_dict(orient="records"):
+        save_prediction(db, record, source="webapp")
+
+    # Return all results as JSON
+    return {"predictions": df.to_dict(orient="records")}
+
+
 
 @app.get("/past-predictions/")
 def past_predictions(source: str = "all", db: Session = Depends(get_db)):
